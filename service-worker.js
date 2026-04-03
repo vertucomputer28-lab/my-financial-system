d0bfc406be830221901e6099e40c9cdd89e011d6
vertucomputer28-lab <vertucomputer28@gmail.com>
2026-04-03 23:38:23 +0000

    Add service worker for offline support

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "finance-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
